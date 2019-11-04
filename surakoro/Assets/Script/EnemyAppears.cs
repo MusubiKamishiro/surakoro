45cdb0fa94b87d8e73f507ba2dbdffac2d306843
kawagoe <hetaresuraimu@icloud.com>
2019-11-04 20:49:11 +0900

    エフェクシアが消えてたので修復

---
+++ surakoro/Assets/Script/EnemyAppears.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAppears : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemyAppearsBox;
+
+    GameObject enemy;
+    // Use this for initialization
+    void Start()
+    {
+        // CubeプレハブをGameObject型で取得
+        enemy = (GameObject)Resources.Load("EnemyBox");
+        // Cubeプレハブを元に、インスタンスを生成、
+        Instantiate(enemy, enemyAppearsBox.transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
