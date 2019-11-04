22120b99725f43a8f41cf9d45aee880706f3d851
Siew Bin <tekito@kk.kk>
2019-11-04 19:59:38 +0900

    WorldMng と SceneMng を作りました。

---
+++ surakoro/Assets/Script/SceneMng.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneMng : MonoBehaviour
+{
+    enum SCENE
+    {
+        TITLE_SCENE,
+        GAME_SCENE,
+        RESULT_SCENE
+    }
+    Button mStartButton;
+    SCENE mScene;
+    SceneManager mSceneMng;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mScene = SCENE.TITLE_SCENE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeScene()
+    {
+        mSceneMng
+    }
+}
