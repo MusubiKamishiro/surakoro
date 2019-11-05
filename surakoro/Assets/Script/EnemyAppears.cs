330d330144edbd2d6ec272245ddf5a404989a7cc
kawagoe <hetaresuraimu@icloud.com>
2019-11-05 09:22:37 +0900

    敵が箱から出てくるように作成

--- surakoro/Assets/Script/EnemyAppears.cs
+++ surakoro/Assets/Script/EnemyAppears.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class EnemyAppears : MonoBehaviour
 {
-    [SerializeField]
-    GameObject enemyAppearsBox;
+    int count;          // 敵が出てくる間の時間
 
-    GameObject enemy;
+    [SerializeField]
+    GameObject enemy;   // 箱から出現させたい敵
+
+    [SerializeField]
+    int count_max;      // 敵が出てくる間の時間（上限）
+
     // Use this for initialization
     void Start()
     {
-        // CubeプレハブをGameObject型で取得
-        enemy = (GameObject)Resources.Load("EnemyBox");
-        // Cubeプレハブを元に、インスタンスを生成、
-        Instantiate(enemy, enemyAppearsBox.transform.position, Quaternion.identity);
+        count = 0;
     }
 
     void Update()
     {
-        
+        count++;
+        // 出るかどうかのためし
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(enemy, transform.position, Quaternion.identity);
+        }
+        // カウントでどれくらいの感覚で出てくるか
+        if(count >= count_max)
+        {
+            // Cubeプレハブを元に、インスタンスを生成
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            count = 0;
+        }
     }
 
 }
