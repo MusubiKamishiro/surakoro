3242a9791ec050da11957cd7a093d6c4fc66ba45
kawagoe <hetaresuraimu@icloud.com>
2019-11-05 20:51:09 +0900

    エフェクトを追加

--- surakoro/Assets/Script/Enemy/EnemyDieOrLife.cs
+++ surakoro/Assets/Script/Enemy/EnemyDieOrLife.cs
@@ -8,18 +8,43 @@ using UnityEngine;
 
 public class EnemyDieOrLife : MonoBehaviour
 {
+    [SerializeField]
+    GameObject effectPrefab;   // 敵が死んだとき、胃袋に飛ばすときのエフェクト
+
+    [SerializeField]
+    GameObject effectGoul;   // 上のエフェクトのゴール
+
+    Vector3 bitween;
+    bool dieFlag;
+    bool stomachInFlag;
+    GameObject effect;
 
     void Start()
     {
+        bitween = Vector3.zero;
+        dieFlag = false;
+        stomachInFlag = false;
     }
 
     void Update()
     {
         // 下に落ちすぎたら
-        if (this.transform.position.y < -10)
+        if (this.transform.position.y < -15 && !dieFlag)
         {
-            // これを消す
-            Destroy(this.gameObject);
+            // これにエフェクトをつける
+            effect = Instantiate(effectPrefab) as GameObject;
+            dieFlag = true;
+        }
+        if(dieFlag && !stomachInFlag)
+        {
+            this.GetComponent<Rigidbody>().useGravity = false;
+            effect.transform.position = this.transform.position;
+            this.transform.position += Vector3.up/2;
+            if((effectGoul.transform.position - this.transform.position).magnitude < 1)
+            {
+                stomachInFlag = true;
+                this.GetComponent<Rigidbody>().useGravity = true;
+            }
         }
     }
 }
