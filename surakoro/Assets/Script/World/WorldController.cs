ae8febbdb4068e5729b9b06f06dbac2455465706
YutaroOno <ono.yutaro.tec0623@gmail.com>
2019-11-05 09:43:42 +0900

    コントローラーのトリガー入力でフィールド全体が回転するようになった。

--- surakoro/Assets/Script/World/WorldController.cs
+++ surakoro/Assets/Script/World/WorldController.cs
@@ -17,8 +17,30 @@ public class WorldController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float triggerVal = Input.GetAxis("L_R_Trigger");
 
+        // トリガーの入力が何もない状態ならIdle
+        if (triggerVal == 0.0f)
+        {
+            mAnim.SetFloat("Trigger", 0.0f);
+        }
+
+        // 右トリガー入力状態なら時計回り回転アニメを開始
+        if (triggerVal >= 0.1f)
+        {
+            mAnim.SetFloat("Trigger", triggerVal);
+            Debug.Log("フィールド:右回転状態");
+            Debug.Log("Trigger;" + triggerVal);
+
+        }
+
+        // 左トリガー入力状態なら反時計回り回転アニメを開始
+        if (triggerVal <= -0.1f)
+        {
+            mAnim.SetFloat("Trigger", triggerVal);
+            Debug.Log("フィールド:左回転状態");
+            Debug.Log("Trigger;" + triggerVal);
+        }
 
     }
 }
