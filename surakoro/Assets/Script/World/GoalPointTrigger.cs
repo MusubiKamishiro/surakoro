f1e8924d55f4490aec7b4ae2018dc78b3bccac2e
YutaroOno <ono.yutaro.tec0623@gmail.com>
2019-11-05 11:37:51 +0900

    StandardAssetsを追加。レベルを更新。デバッグ用ゴール地点を設置

---
+++ surakoro/Assets/Script/World/GoalPointTrigger.cs
@@ -0,0 +1,21 @@
+﻿//---------------------------------------------------------------------//
+// ゴール地点の当たり判定(Playerが接触したらリザルトへ) Debug
+//                                       Yutaro Ono.
+//--------------------------------------------------------------------//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalPointTrigger : MonoBehaviour
+{
+
+    // プレイヤーが触れたらリザルトへ
+    private void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("ResultScene");
+        }
+    }
+}
