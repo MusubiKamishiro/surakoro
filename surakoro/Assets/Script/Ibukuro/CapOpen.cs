79bb1b2b6f7b20739e7252745f67c0c98b71a7a5
kawagoe <hetaresuraimu@icloud.com>
2019-11-06 21:36:52 +0900

    蓋を開けたり閉めたりするやつ追加 特定のオブジェクトの重力を変化させるプログラムを追加

---
+++ surakoro/Assets/Script/Ibukuro/CapOpen.cs
@@ -0,0 +1,33 @@
+﻿//***************************************
+// CapOpen.cs
+// capにつけるプログラム
+//***************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapOpen : MonoBehaviour
+{
+    // 外部から読み込み
+    [SerializeField]
+    GameObject leftCap;
+    [SerializeField]
+    GameObject rightCap;
+
+    // ここでしか使わない変数
+    private CapSensor capSensor;    // フラグ呼び出し用
+    void Start()
+    {
+        // capSensorのインスタンスを見つける
+        capSensor = FindObjectOfType<CapSensor>();
+    }
+
+    void Update()
+    {
+        // 蓋が開くフラグがたったら
+        if (capSensor.OpenFlag)
+        {
+            
+        }
+    }
+}
---
+++ surakoro/Assets/Script/Ibukuro/Ibukuro.cs
@@ -0,0 +1,27 @@
+﻿//***************************************
+// Ibukuro.cs
+// Ibukuroにつけるプログラム
+//***************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ibukuro : MonoBehaviour
+{
+    // ここでしか使わない変数
+    private CapSensor capSensor;    // フラグ呼び出し用
+    void Start()
+    {
+        // capSensorのインスタンスを見つける
+        capSensor = FindObjectOfType<CapSensor>();
+    }
+
+    void Update()
+    {
+        // ボーナスに入ったら
+        if(capSensor.BonusFlag)
+        {
+            //ボーナス要素を追加したい（Scoreを増やす等）
+        }
+    }
+}
