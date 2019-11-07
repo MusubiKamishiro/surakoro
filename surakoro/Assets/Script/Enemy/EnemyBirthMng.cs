af186aa4b333923f53a900ea886639bf71566733
Siew Bin <tekito@kk.kk>
2019-11-07 09:33:12 +0900

    インスペクターに値を変えるように作りました。

--- surakoro/Assets/Script/Enemy/EnemyBirthMng.cs
+++ surakoro/Assets/Script/Enemy/EnemyBirthMng.cs
@@ -1,37 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyBirthMng : MonoBehaviour
 {
+
     [SerializeField]
-    GameObject[] enemyBirthGroup;
-    private bool[] normalValueFlag;
+    private GameObject[] enemyBirthGroup;
     [SerializeField]
     Player player;
-    private int slowSpawnRate = 3;
+    private bool[] revertValueFlag;
+    [SerializeField]
+    private float[] slowSpawnRate;
+    [SerializeField]
+    private int[] lowerEnemyMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        normalValueFlag = new bool[enemyBirthGroup.Length];
-        normalValueFlag[0] = false;
+        revertValueFlag = new bool[enemyBirthGroup.Length];
+        revertValueFlag[0] = false;
 
         //enemyBirthGroup = new GameObject[wallNum];
         for (int j = 1; j < enemyBirthGroup.Length; j++)
         {
-            normalValueFlag[j] = false;
-
+            revertValueFlag[j] = false;
 
             for (int i = 0; i < enemyBirthGroup[j].transform.childCount; i++)
             {
-
-                enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().SetCountMax(enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().GetCountMax() * slowSpawnRate);
-                enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().SetEnemyMax(enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().GetEnemyMax() / 2);
-                if (j + 1 < enemyBirthGroup.Length)
-                {
-                    enemyBirthGroup[j + 1].SetActive(false);
-                }
+                enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().SetCountMax(enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().GetCountMax() * slowSpawnRate[j]);
+                enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().SetEnemyMax(enemyBirthGroup[j].transform.GetChild(i).gameObject.GetComponent<EnemyBirth>().GetEnemyMax() / lowerEnemyMax[j]);
+               
+            }
+            if (j + 1 < enemyBirthGroup.Length)
+            {
+                enemyBirthGroup[j + 1].SetActive(false);
             }
         }
     }
@@ -46,26 +47,22 @@ public class EnemyBirthMng : MonoBehaviour
     {
         for (int i = 1; i < enemyBirthGroup.Length; ++i)
         {
-            if (!player.GetGiantFlag(i - 1) && !normalValueFlag[i])
+            if (!player.GetGiantFlag(i - 1) && !revertValueFlag[i])
             {
                 for (int j = 0; j < enemyBirthGroup[i].transform.childCount; j++)
                 {
-                    enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().SetCountMax(enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().GetCountMax() / slowSpawnRate);
-                    enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().SetEnemyMax(enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().GetEnemyMax() * 2);
+                    enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().SetCountMax(enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().GetCountMax() / slowSpawnRate[j]);
+                    enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().SetEnemyMax(enemyBirthGroup[i].transform.GetChild(j).gameObject.GetComponent<EnemyBirth>().GetEnemyMax() * lowerEnemyMax[j]);
                 }
                 if (i + 1 < enemyBirthGroup.Length)
                 {
                     enemyBirthGroup[i + 1].SetActive(true);
                 }
                 enemyBirthGroup[i - 1].SetActive(false);
-                normalValueFlag[i] = true;
+                revertValueFlag[i] = true;
 
             }
 
-            //else
-            //{
-            //    level[i]
-            //}
         }
     }
 }
